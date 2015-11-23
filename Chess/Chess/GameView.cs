70d393e14e4ee75969c59c658ab99eb682cd7f21
nilselveros <nilselveros@gmail.com>
2015-11-23 19:27:32 +0100

    added rulesengine

--- Chess/Chess/GameView.cs
+++ Chess/Chess/GameView.cs
@@ -11,16 +11,20 @@ namespace Chess
         Player playerblack = new CPUPlayer();
         Player playerwhite = new HumanPlayer();
         ChessBoard board;
+        RulesEngine rules;
+        bool p1turn = true;
         public GameView()
         {
             board = new ChessBoard(playerwhite, playerblack);
+            rules = new RulesEngine(board);
         }
 
         //Kallas på efter att en player försöker göra ett drag för att se till att det är giltigt
         public void makeMove(int fromX, int fromY, int toX, int toY)
         {
+            
             Move move = new Move(fromX, fromY, toX, toY);
-            if (board.isValid(move) == true)
+            if (rules.isValid(move))
             {
                 //gui.updateBoard();
                 board.updateTable(move);
