43e5407693ff5e7e9af38f678c4c959dd90865e4
Marcus Fridén <marfr665@student.liu.se>
2015-12-15 15:58:55 +0100

    Added data storage

---
+++ Chess2.0/Chess2.0/DataStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess2._0
+{
+    class DataStorage
+    {
+        public DataStorage(){}
+
+        public void LoadData()
+        {
+
+        }
+
+        public void SaveData(ChessPiece[,] pieces)
+        {
+            
+        }
+    }
+}
