e55180fe9121d8c20932304ed7a8f9db9b32ffd8
nilselveros <nilselveros@gmail.com>
2016-01-20 02:17:08 +0100

    fix med xml filen

--- Chess2.0/Chess2.0/DataStorage.cs
+++ Chess2.0/Chess2.0/DataStorage.cs
@@ -13,6 +13,7 @@ namespace Chess2._0
   public class DataStorage
     {
         FileSystemWatcher watcher;
+        bool fileInUse = false;
 
         public DataStorage(FileSystemWatcher watch)
         {
@@ -22,88 +23,110 @@ namespace Chess2._0
         //Läser data från xml-fil, skapar en representation av schackbräädet och returnerar det.
         public ChessPiece[,] LoadData()
         {
-            ChessPiece[,] board = new ChessPiece[8, 8];
-            XDocument doc = XDocument.Load(@".\chessdata\chessdata.xml");
-
-            var data = from item in doc.Descendants("ChessPiece")
-                       select new
-                       {
-                           posx = item.Element("posX").Value,
-                           posy = item.Element("posY").Value,
-                           color = item.Element("color").Value,
-                           type = item.Element("type").Value
-                       };
-
-            foreach (var c in data)
+            if (!fileInUse)
             {
-                switch (c.type)
+                fileInUse = true;
+                ChessPiece[,] board = new ChessPiece[8, 8];
+                XDocument doc = XDocument.Load(@".\chessdata\chessdata.xml");
+
+                var data = from item in doc.Descendants("ChessPiece")
+                           select new
+                           {
+                               posx = item.Element("posX").Value,
+                               posy = item.Element("posY").Value,
+                               color = item.Element("color").Value,
+                               type = item.Element("type").Value
+                           };
+
+                foreach (var c in data)
                 {
-                    case "Chess2._0.King":
-                        ChessPiece king = new King(c.color, int.Parse(c.posx), int.Parse(c.posy));
-                        board[int.Parse(c.posx), int.Parse(c.posy)] = king;
-                        break;
-                    case "Chess2._0.Queen":
-                        ChessPiece queen = new Queen(c.color, int.Parse(c.posx), int.Parse(c.posy));
-                        board[int.Parse(c.posx), int.Parse(c.posy)] = queen;
-                        break;
-                    case "Chess2._0.Runner":
-                        ChessPiece runner = new Runner(c.color, int.Parse(c.posx), int.Parse(c.posy));
-                        board[int.Parse(c.posx), int.Parse(c.posy)] = runner;
-                        break;
-                    case "Chess2._0.Horse":
-                        ChessPiece horse = new Horse(c.color, int.Parse(c.posx), int.Parse(c.posy));
-                        board[int.Parse(c.posx), int.Parse(c.posy)] = horse;
-                        break;
-                    case "Chess2._0.Tower":
-                        ChessPiece tower = new Tower(c.color, int.Parse(c.posx), int.Parse(c.posy));
-                        board[int.Parse(c.posx), int.Parse(c.posy)] = tower;
-                        break;
-                    case "Chess2._0.Farmer":
-                        ChessPiece farmer = new Farmer(c.color, int.Parse(c.posx), int.Parse(c.posy));
-                        board[int.Parse(c.posx), int.Parse(c.posy)] = farmer;
-                        break;
+                    switch (c.type)
+                    {
+                        case "Chess2._0.King":
+                            ChessPiece king = new King(c.color, int.Parse(c.posx), int.Parse(c.posy));
+                            board[int.Parse(c.posx), int.Parse(c.posy)] = king;
+                            break;
+                        case "Chess2._0.Queen":
+                            ChessPiece queen = new Queen(c.color, int.Parse(c.posx), int.Parse(c.posy));
+                            board[int.Parse(c.posx), int.Parse(c.posy)] = queen;
+                            break;
+                        case "Chess2._0.Runner":
+                            ChessPiece runner = new Runner(c.color, int.Parse(c.posx), int.Parse(c.posy));
+                            board[int.Parse(c.posx), int.Parse(c.posy)] = runner;
+                            break;
+                        case "Chess2._0.Horse":
+                            ChessPiece horse = new Horse(c.color, int.Parse(c.posx), int.Parse(c.posy));
+                            board[int.Parse(c.posx), int.Parse(c.posy)] = horse;
+                            break;
+                        case "Chess2._0.Tower":
+                            ChessPiece tower = new Tower(c.color, int.Parse(c.posx), int.Parse(c.posy));
+                            board[int.Parse(c.posx), int.Parse(c.posy)] = tower;
+                            break;
+                        case "Chess2._0.Farmer":
+                            ChessPiece farmer = new Farmer(c.color, int.Parse(c.posx), int.Parse(c.posy));
+                            board[int.Parse(c.posx), int.Parse(c.posy)] = farmer;
+                            break;
+                    }
                 }
+
+                fileInUse = false;
+                return board;
             }
-            return board;
+            else
+            {
+                LoadData();
+            }
+            return null;
         }
+        
 
         //Skriver schackbrädet i sitt nuvarande tillstånd till en xml-fil
         public void SaveData(ChessPiece[,] board)
         {
             watcher.EnableRaisingEvents = false;
-
-            ArrayList list = new ArrayList();
-            for (int x = 0; x < board.GetLength(0); x++)
+            if (!fileInUse)
             {
-                for (int y = 0; y < board.GetLength(1); y++)
+
+                fileInUse = true;
+
+                ArrayList list = new ArrayList();
+                for (int x = 0; x < board.GetLength(0); x++)
                 {
-                    if(board[x, y] != null)
+                    for (int y = 0; y < board.GetLength(1); y++)
                     {
-                        list.Add(board[x, y]);
+                        if (board[x, y] != null)
+                        {
+                            list.Add(board[x, y]);
+                        }
                     }
                 }
+
+                //gör om arraylist till array
+                ChessPiece[] listan = list.ToArray(typeof(ChessPiece)) as ChessPiece[];
+
+                XDocument xmlDoc = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "yes"),
+
+                new XComment("Nils och Fridens coola xml doc!"),
+
+                new XElement("ChessPieces",
+                from ChessPiece in listan
+
+                select new XElement("ChessPiece", new XElement("posX", ChessPiece.posX),
+                                                  new XElement("posY", ChessPiece.posY),
+                                                  new XElement("color", ChessPiece.Color),
+                                                  new XElement("type", ChessPiece.GetType())
+
+            )));
+                xmlDoc.Save(@".\chessdata\chessdata.xml");
+
+                watcher.EnableRaisingEvents = true;
+                fileInUse = false;
             }
-            
-            //gör om arraylist till array
-            ChessPiece[] listan = list.ToArray(typeof(ChessPiece)) as ChessPiece[]; 
-
-            XDocument xmlDoc = new XDocument(
-            new XDeclaration("1.0", "UTF-8", "yes"),
-
-            new XComment("Nils och Fridens coola xml doc!"),
-
-            new XElement("ChessPieces",
-            from ChessPiece in listan
-   
-            select new XElement("ChessPiece", new XElement("posX", ChessPiece.posX),
-                                              new XElement("posY", ChessPiece.posY), 
-                                              new XElement("color", ChessPiece.Color), 
-                                              new XElement("type", ChessPiece.GetType())
-
-        )));
-            xmlDoc.Save(@".\chessdata\chessdata.xml");
-
-            watcher.EnableRaisingEvents = true;
+            else
+            {
+                SaveData(board);
+            }
         }
 
         //Ta bort xml-filen
--- Chess2.0/Chess2.0/GameView.cs
+++ Chess2.0/Chess2.0/GameView.cs
@@ -115,6 +115,7 @@ namespace Chess2._0
         //Kallas på efter att en player försöker göra ett drag för att se till att det är giltigt
         public void makeMove(int fromX, int fromY, int toX, int toY)
         {         
+            
             Move move = new Move(fromX, fromY, toX, toY);
 
             if (rules.isValid(move, gamestatus, board.get()))
@@ -163,7 +164,7 @@ namespace Chess2._0
 
         private void xmlwatcher(object source, FileSystemEventArgs e)
         {
-    
+            
             watcher.EnableRaisingEvents = false;
             board.setBoard(ds.LoadData());
             Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate
